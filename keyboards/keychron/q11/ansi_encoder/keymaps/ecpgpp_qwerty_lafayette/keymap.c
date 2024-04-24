f0ac3ef382f8064c90aabff1f315c98a5b289de2
il-as <iliaselbssiri@gmail.com>
2024-04-24 02:11:49 +0200

    feat(Keymap): add new key to reach alt_gr layer add new positions for *-+ characters in alt_gr layer add super secret macro to do 2328 0066 006c 0069 0028 256f 00b0 25a1 00b0 0029 256f fe35 0020 253b 2501 253b hahahahahah convert it sucker

--- keyboards/keychron/q11/ansi_encoder/keymaps/ecpgpp_qwerty_lafayette/keymap.c
+++ keyboards/keychron/q11/ansi_encoder/keymaps/ecpgpp_qwerty_lafayette/keymap.c
@@ -51,6 +51,7 @@ enum custom_keycodes {
     US_YDIA, // ÿ
     US_HDIA, // ḧ
     US_XDIA, // ẍ
+    US_FLIP,
     US_IDIA = ALGR(KC_J), // ï
     US_EDIA = ALGR(KC_F), // ë
 
@@ -61,6 +62,12 @@ enum custom_keycodes {
 
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
+
+// ucis
+const ucis_symbol_t ucis_symbol_table[] = {
+    UCIS_SYM("flip", 0x0028, 0x256F, 0x00B0, 0x25A1, 0x00B0, 0x0029, 0x256F, 0xFE35, 0x0020, 0x253B, 0x2501, 0x253B)
+};
+
 
 // dead key conversion from us intl multi deadkey setup to lafayette's single dead key
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -170,6 +177,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code(US_X);
             }
             break;
+        case US_FLIP:
+            if(record->event.pressed) {
+                //SEND_STRING("(╯°□°)╯︵ ┻━┻");
+                ucis_start();
+                send_unicode_string("flip");
+                for (int i = 0; i < strlen("flip"); i++) {
+                    tap_code(KC_BSPC);
+                }
+            }
+            break;
     }
     return true;
 };
@@ -233,10 +250,10 @@ const key_override_t **key_overrides = (const key_override_t *[]) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_91_ansi(
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
-        _______,  US_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
+        US_FLIP,  US_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         _______,  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     OSL(DK1), US_QUOT,              KC_ENT,             KC_HOME,
-        _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+     MO(ALT_GR),  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LOPT,  KC_LCMD,  MO(MAC_FN),         KC_SPC,                        KC_BSPC,          MO(ALT_GR), MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
     /* NOTE: MAC_BASE layer
      * ┌───┐ ┌───┬───┬───┬───┬───┬───┬───┐        ┌───┬───┬───┬───┬───┬───┬───┬───┐ ┌───┐
@@ -249,9 +266,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┤ ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┐       └┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ ├───┤
      * │M3 │ │Caps  │ A │ S │ D │ F │ G │        │ H │ J │ K │ L │DK2│ ' │    Ent │ │HOM│
      * ├───┤ ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┐      └─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬─┬─┴─┼───┘
-     * │M4 │ │Sft     │ Z │ X │ C │ V │ B │        │ N │ M │ , │ . │ / │Shift │ │Up │
+     * │AlG│ │Sft     │ Z │ X │ C │ V │ B │        │ N │ M │ , │ . │ / │Shift │ │Up │
      * ├───┤ ├────┬───┴┬──┴─┬─┴──┬┴───┴───┤        ├───┴───┴──┬┴──┬┴──┬┴──┬─┬─┴─┼───┼───┐
-     * │M5 │ │Ctrl│GUI │CMD │ FN │Space   │        │Bkspc Del │Alt│Fn1│Ctr│ │Lft│Dwn│Rig│
+     * │M5 │ │Ctrl│GUI │CMD │ FN │Space   │        │Bkspc Del │AlG│Fn1│Ctr│ │Lft│Dwn│Rig│
      * └───┘ └────┴────┴────┴────┴────────┘        └──────────┴───┴───┴───┘ └───┴───┴───┘
      */
 
@@ -259,7 +276,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_TOG,  _______,  KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,  _______,  RGB_TOG,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
         _______,  RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,   _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,            _______,
-        _______,  _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,              _______,            _______,
+        _______,  _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXX,  _______,              _______,            _______,
         _______,  _______,            _______,  _______,  _______,  _______,   _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,  _______,
         _______,  _______,  _______,  _______,  _______,            _______,                       _______,            _______,  _______,    _______,  _______,  _______,  _______),
      /* NOTE: MAC_FN layer
@@ -281,10 +298,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [DK1] = LAYOUT_91_ansi(
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
-        _______,  KC_NO,    KC_NO,    US_LDQU,  US_RDQU,  US_CENT,  US_PND,    US_YEN,   US_CURR,  US_SECT,  US_PILC,  US_DEG,   KC_NO,      KC_EQL,   KC_BSPC,            KC_PGUP,
+        US_FLIP,  KC_NO,    KC_NO,    US_LDQU,  US_RDQU,  US_CENT,  US_PND,    US_YEN,   US_CURR,  US_SECT,  US_PILC,  US_DEG,   KC_NO,      KC_EQL,   KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   US_AE,    US_EACU,  US_EGRV,  US_EDIA,  US_MINS,   US_UDIA,  US_UGRV,  US_IDIA,  US_OE,    KC_NO,    KC_NO,      KC_NO,    KC_NO,              KC_PGDN,
         _______,  KC_CAPS,  US_AGRV,  US_SS,    US_ECFX,  US_MINS,  KC_NO,     US_YCFX,  US_UCFX,  US_ICFX,  US_OCFX,  OSL(DK2), KC_NO,                KC_ENT,             KC_HOME,
-        _______,  KC_LSFT,            US_ACFX,  US_MUL,   US_CCED,  US_UNDS,   KC_NO,    US_NTIL,  US_MICR,  US_ITPT,  KC_NO,    US_DIV,               KC_RSFT,  KC_UP,
+     MO(ALT_GR),  KC_LSFT,            US_ACFX,  US_MUL,   US_CCED,  US_UNDS,   KC_NO,    US_NTIL,  US_MICR,  US_ITPT,  KC_NO,    US_DIV,               KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LOPT,  KC_LCMD,  MO(MAC_FN),         KC_SPC,                        KC_BSPC,            KC_RALT,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
     /* NOTE: Dead Key 1 layer
      * ┌───┐ ┌───┬───┬───┬───┬───┬───┬───┐        ┌───┬───┬───┬───┬───┬───┬───┬───┐ ┌───┐
@@ -297,9 +314,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┤ ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┐       └┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ ├───┤
      * │M3 │ │Caps  │ à │ ß │ Ê │ - │   │        │ Ŷ │ Û │ Î │ Ô │DK2│ ' │    Ent │ │HOM│
      * ├───┤ ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┐      └─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬─┬─┴─┼───┘
-     * │M4 │ │Sft     │ Â │ × │ Ç │ _ │   │        │ Ñ │ µ │ · │   │ ÷¿│Shift │ │Up │
+     * │AlG│ │Sft     │ Â │ × │ Ç │ _ │   │        │ Ñ │ µ │ · │   │ ÷¿│Shift │ │Up │
      * ├───┤ ├────┬───┴┬──┴─┬─┴──┬┴───┴───┤        ├───┴───┴──┬┴──┬┴──┬┴──┬─┬─┴─┼───┼───┐
-     * │M5 │ │Ctrl│GUI │CMD │ FN │Space   │        │Bkspc Del │Cmd│Fn1│Ctr│ │Lft│Dwn│Rig│
+     * │M5 │ │Ctrl│GUI │CMD │ FN │Space   │        │Bkspc Del │Alt│Fn1│Ctr│ │Lft│Dwn│Rig│
      * └───┘ └────┴────┴────┴────┴────────┘        └──────────┴───┴───┴───┘ └───┴───┴───┘
      * For now, capitalised versions of è à ù need to be done with caps lock.
      */
@@ -309,7 +326,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,      KC_NO,    KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   KC_NO,    US_WDIA,  US_EDIA,  KC_NO,    US_TDIA,   US_YDIA,  US_UDIA,  US_IDIA,  US_ODIA,  KC_NO,    KC_NO,      KC_NO,    KC_NO,              KC_PGDN,
         _______,  KC_CAPS,  US_ADIA,  KC_NO,    KC_NO,    KC_NO,    KC_NO,     US_HDIA,  KC_NO,    KC_NO,    KC_NO,    KC_SCLN,  KC_NO,                KC_ENT,             KC_HOME,
-        _______,  KC_LSFT,            KC_NO,    US_XDIA,  KC_NO,    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                KC_RSFT,  KC_UP,
+     MO(ALT_GR),  KC_LSFT,            KC_NO,    US_XDIA,  KC_NO,    KC_NO,     KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LOPT,  KC_LCMD,  MO(MAC_FN),         KC_SPC,                        KC_BSPC,            KC_RALT,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
     /* INFO: Dead Key 2 layer
      * ┌───┐ ┌───┬───┬───┬───┬───┬───┬───┐        ┌───┬───┬───┬───┬───┬───┬───┬───┐ ┌───┐
@@ -332,10 +349,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_BASE] = LAYOUT_91_ansi(
         KC_MUTE,  KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_INS,   KC_DEL,   KC_MUTE,
-        _______,  US_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
+        US_FLIP,  US_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         _______,  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
         _______,  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     OSL(DK1), US_QUOT,              KC_ENT,             KC_HOME,
-        _______,  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
+     MO(ALT_GR),  KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LWIN,  KC_LALT,  MO(WIN_FN),         KC_SPC,                        KC_BSPC,          MO(ALT_GR), MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
     /* INFO: WIN_BASE layer
      * ┌───┐ ┌───┬───┬───┬───┬───┬───┬───┐        ┌───┬───┬───┬───┬───┬───┬───┬───┐ ┌───┐
@@ -380,9 +397,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [ALT_GR] = LAYOUT_91_ansi(
         KC_MUTE,  KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_INS,   KC_DEL,   KC_MUTE,
-        _______,  KC_NO,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
-        _______,  KC_TAB,   KC_AT,    KC_LT,    KC_GT,    KC_DLR,   KC_PERC,   US_CIRC,  KC_AMPR,  KC_PAST,  US_QUOT,  US_GRV,   KC_NO,      KC_NO,    KC_NO,              KC_PGDN,
-        _______,  KC_CAPS,  KC_LCBR,  KC_LPRN,  KC_RPRN,  KC_RCBR,  KC_EQL,    KC_BSLS,  US_PLUS,  KC_PMNS,  KC_SLSH,  US_DQUO,  KC_NO,                KC_ENT,             KC_HOME,
+        US_PLUS,  KC_NO,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
+        KC_PMNS,  KC_TAB,   KC_AT,    KC_LT,    KC_GT,    KC_DLR,   KC_PERC,   US_CIRC,  KC_AMPR,  KC_PAST,  US_QUOT,  US_GRV,   KC_NO,      KC_NO,    KC_NO,              KC_PGDN,
+        KC_PAST,  KC_CAPS,  KC_LCBR,  KC_LPRN,  KC_RPRN,  KC_RCBR,  KC_EQL,    KC_BSLS,  US_PLUS,  KC_PMNS,  KC_SLSH,  US_DQUO,  KC_NO,                KC_ENT,             KC_HOME,
         _______,  KC_LSFT,            US_TILD,  KC_LBRC,  KC_RBRC,  KC_UNDS,  KC_HASH,   KC_PIPE,  KC_EXLM,  US_SCLN,  US_COLN,  US_QUES,              KC_RSFT,  KC_UP,
         _______,  KC_LCTL,  KC_LGUI,  KC_LCMD,  MO(MAC_FN),         KC_SPC,                        KC_BSPC,            KC_RALT,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
      /* INFO : ALT_GR (dev special chars) layer
@@ -390,11 +407,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       * │MUT│ │Esc│F1 │F2 │F3 │F4 │F5 │F6 │        │F7 │F8 │F9 │F10│F11│F12│Ins│Del│ │MUT│
       * └───┘ └───┴───┴───┴───┴───┴───┴───┘        └───┴───┴───┴───┴───┴───┴───┴───┘ └───┘
       * ┌───┐ ┌───┬───┬───┬───┬───┬───┬───┐        ┌───┬───┬───┬───┬───┬───┬───────┐ ┌───┐
-      * │   │ │   │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │        │ 7 │ 8 │ 9 │ 0 │ - │ = │Bkspc  │ │PGU│
+      * │ + │ │   │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │        │ 7 │ 8 │ 9 │ 0 │ - │ = │Bkspc  │ │PGU│
       * ├───┤ ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┘      ┌─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┤
-      * │   │ │Tab  │ @ │ < │ > │ $ │ % │        │ ^ │ & │ *×│ ' │ ` │   │   │     │ │PGD│
+      * │ - │ │Tab  │ @ │ < │ > │ $ │ % │        │ ^ │ & │ *×│ ' │ ` │   │   │     │ │PGD│
       * ├───┤ ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┐       └┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ ├───┤
-      * │   │ │Caps  │ { │ ( │ ) │ } │ = │        │ \ │ +±│ - │ /÷│ " │   │    Ent │ │HOM│
+      * │ * │ │Caps  │ { │ ( │ ) │ } │ = │        │ \ │ +±│ - │ /÷│ " │   │    Ent │ │HOM│
       * ├───┤ ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┐      └─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬─┬─┴─┼───┘
       * │   │ │Sft     │ ~ │ [ │ ] │ _ │ # │        │ |¦│ ! │ ; │ : │ ? │Shift │ │Up │
       * ├───┤ ├────┬───┴┬──┴─┬─┴──┬┴───┴───┤        ├───┴───┴──┬┴──┬┴──┬┴──┬─┬─┴─┼───┼───┐
